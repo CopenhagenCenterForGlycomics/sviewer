a31c49cd84b83804bf6a649015993ff01ef90bb1
Hiren Joshi <hirenj@gmail.com>
2018-04-25 22:48:43 +0200

    Automatically add in filter element

--- js/sugarframe.js
+++ js/sugarframe.js
@@ -18,6 +18,8 @@ function WrapHTML() { return Reflect.construct(HTMLElement, [], Object.getProtot
 Object.setPrototypeOf(WrapHTML.prototype, HTMLElement.prototype);
 Object.setPrototypeOf(WrapHTML, HTMLElement);
 
+const highlight_filter = Symbol('highlight');
+
 const tmpl = document.createElement('template');
 
 tmpl.innerHTML = `
@@ -25,10 +27,16 @@ tmpl.innerHTML = `
   :host {
     display: block;
     position: relative;
+    --highlight-stroke: #000;
+    --highlight-stroke-opacity: 1;
+    --highlight-fill-opacity: 1;
+    --highlight-fill: #fff;
   }
+
   :host div {
     display: none;
   }
+
   :host .widget_contents {
     width: 100%;
     height: 100%;
@@ -43,15 +51,81 @@ tmpl.innerHTML = `
 <object id="targetsvg" type="image/svg+xml"></object>
 `;
 
+const FILTER_TEXT = `
+  <feColorMatrix mode="matrix" in="SourceGraphic" values="0 0 0 0 0
+                                                          0 0 0 0 0
+                                                          0 0 0 0 0
+                                                          0 0 0 1 0" result="blacksource"/>
+  <feGaussianBlur in="blacksource" stdDeviation="10" result="blur" />
+  <feColorMatrix mode="matrix" in="blur" values="0 0 0 0 0
+                                                 0 0 0 0 0
+                                                 0 0 0 0 0
+                                                 0 0 0 30 -3" result="contrastup"/>
+  <feMorphology operator="dilate" in="contrastup" radius="10"/>
+  <feColorMatrix mode="matrix"           values="0 0 0 0 1
+                                                 0 0 0 0 1
+                                                 0 0 0 0 1
+                                                 0 0 0 0.3 0" result="feather_back"/>
+  <feMorphology operator="dilate" in="feather_back" radius="10"/>
+  <feColorMatrix mode="matrix" values="0 0 0 0 0.1
+                                       0 0 0 0 0.1
+                                       0 0 0 0 0.1
+                                       0 0 0 1 0" result="outeroutline"/>
+  <feComposite in2="outeroutline" in="feather_back" result="outline"/>
+  <feComposite in2="outline" in="SourceGraphic"/>
+`;
+
 if (window.ShadyCSS) {
   ShadyCSS.prepareTemplate(tmpl, 'x-sugarframe');
 }
 
-let copy_styles = function() {
+const hex_to_component = (hex) => {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+  } : null;
+};
+
+const mixin_colours = (hex,opacity) => {
+  let c = hex_to_component(hex.replace(/^\s+/,''));
+  return `0 0 0 0 ${(c.r/255).toFixed(1)} 0 0 0 0 ${(c.g/255).toFixed(1)} 0 0 0 0 ${(c.b/255).toFixed(1)} 0 0 0 ${opacity} 0`;
+};
+
+const update_highlight_colours = function() {
+  let filter = this[highlight_filter];
+  let actual_style = window.getComputedStyle(this);
+  let stroke = actual_style.getPropertyValue('--highlight-stroke');
+  let fill = actual_style.getPropertyValue('--highlight-fill');
+  let stroke_opacity = actual_style.getPropertyValue('--highlight-stroke-opacity');
+  let fill_opacity = actual_style.getPropertyValue('--highlight-fill-opacity');
+  filter.querySelector('feColorMatrix[result="outeroutline"]').setAttribute('values',mixin_colours(stroke,stroke_opacity));
+  filter.querySelector('feColorMatrix[result="feather_back"]').setAttribute('values',mixin_colours(fill,fill_opacity));
+};
+
+const make_filter = function(svg) {
+  const filter = svg.createElementNS('http://www.w3.org/2000/svg','filter');
+  filter.innerHTML = FILTER_TEXT;
+  filter.setAttribute('id','highlight');
+  filter.setAttribute('filterUnits','objectBoundingBox');
+  this[highlight_filter] = filter;
+  svg.documentElement.appendChild(filter);
+  update_highlight_colours.call(this);
+};
+
+const copy_styles = function() {
   let svg = this.shadowRoot.querySelector('object').contentDocument;
   var styleElement = svg.createElementNS('http://www.w3.org/2000/svg', 'style');
   styleElement.textContent = this.shadowRoot.querySelector('slot').assignedNodes().map( node => node.textContent ).join('\n');
   svg.documentElement.appendChild(styleElement);
+  make_filter.call(this,svg);
 };
 
 class SugarFrame extends WrapHTML {
@@ -99,7 +173,7 @@ class SugarFrame extends WrapHTML {
     let tag_symbol = Symbol('supported');
     this.renderer.groupTag = tag_symbol;
     this.renderer.sugars.forEach( sug => {
-      this.reactiongroup.supportLinkages(sug,this.reactiongroup.reactions.slice(1,60),tag_symbol);
+      this.reactiongroup.supportLinkages(sug,this.reactiongroup.reactions,tag_symbol);
     });
     this.renderer.refresh();
   }
