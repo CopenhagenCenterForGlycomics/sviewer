7f08276aaab9bb2fdb4bd5c6caf6ec9d0fe301de
Hiren Joshi <hirenj@gmail.com>
2019-07-01 16:24:17 +0200

    Add start of autorepeat

---
+++ js/autorepeat.js
@@ -0,0 +1,14 @@
+// import { CondensedIupac, Sugar, Repeat, Reaction } from 'glycan.js';
+
+// const LACNAC = new IupacSugar();
+// LACNAC.sequence = 'Gal(b1-?)GlcNAc(b1-?)*';
+
+// let matches = sug.match_sugar_pattern(LACNAC, Reaction.Comparator );
+// if (matches.length > 0) {
+//   matches[0].composition().forEach( traced => traced.original.setTag(tag_symbol) );
+// }
+
+
+const do_repeats = () => {};
+
+export default do_repeats;
