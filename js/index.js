2df73ca0d0958777e0887d378dbbe0317a78c1f6
Hiren Joshi <hirenj@gmail.com>
2020-09-27 19:44:59 +0200

    Split commonjs and modular codepaths

--- js/index.js
+++ js/index.js
@@ -1,8 +1,2 @@
-import * as SViewer from './sviewer';
-import * as SugarBuilder from './builder';
-import * as SugarFrame from './sugarframe';
-
-
-export {SViewer as SViewer};
-export {SugarBuilder as SugarBuilder};
-export {SugarFrame as SugarFrame};
+/*global */
+module.exports = require('esm')(module)('./main-node.js');
---
+++ js/main.js
@@ -0,0 +1,6 @@
+import * as SViewer from './sviewer';
+import * as SugarBuilder from './builder';
+import * as SugarFrame from './sugarframe';
+
+
+export {SViewer, SugarBuilder, SugarFrame};
