3cc3d1ab084ea24f22ff6e69fc239f2117cf605b
Hiren Joshi <hirenj@gmail.com>
2021-09-24 13:16:56 +0200

    Enable rendering of single sequence fragments

--- js/main-node.js
+++ js/main-node.js
@@ -44,19 +44,22 @@ class BrowserShimmedSVGRenderer extends SVGRenderer {
 
       for (let res of sugar.composition()) {
         let element = this.rendered.get(res).residue.element;
-        let transform = element.getAttribute('transform').trim();
+        if (element.hidden) {
+          continue;
+        }
+        let transform = element.getAttribute('transform').replace(/rotate\([^\)]*\)/,'').trim();
         let [translate,scale] = transform.split(/\s+/).map( el => el.replace(/[^\d\.\-,]/g,'').split(',').map(parseFloat));
-        if (min.x == null || translate[0] < min.x) {
+        if (min.x == null || (translate[0] < min.x)) {
           min.x = translate[0];
         }
-        if (max.x == null || translate[0] + scale[0] > max.x ) {
+        if (max.x == null || ((translate[0] + scale[0]) > max.x) ) {
           max.x = translate[0] + scale[0];
         }
 
-        if (min.y == null || translate[1] < min.y) {
+        if (min.y == null || (translate[1] < min.y)) {
           min.y = translate[1];
         }
-        if (max.y == null || translate[1] + scale[1] > max.y ) {
+        if (max.y == null || ((translate[1] + scale[1]) > max.y) ) {
           max.y = translate[1] + scale[1];
         }
       }
@@ -84,7 +87,7 @@ const Iupac = CondensedIupac.IO;
 const IupacSugar = Mass(Iupac(Sugar));
 
 
-async function render_iupac_sugar(sequence='Man(a1-3)Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-O)Ser',options={linkage:false,oxford:false}) {
+function parse_sequences(sequence) {
   let is_array_input = Array.isArray(sequence);
   if (! Array.isArray(sequence)) {
     sequence = [sequence];
@@ -94,33 +97,109 @@ async function render_iupac_sugar(sequence='Man(a1-3)Man(b1-4)GlcNAc(b1-4)GlcNAc
     sugar.sequence = seq;
     return sugar;
   });
-  let renderer = new BrowserShimmedSVGRenderer(options.oxford ? LinkageLayout : SugarAwareLayout);
+  return sugars;
+}
+
+function create_renderer_for_sugars(sugars, layout=SugarAwareLayout) {
+  let renderer = new BrowserShimmedSVGRenderer(layout);
   for (let sugar of sugars) {
     renderer.addSugar(sugar);
   }
+  return renderer;  
+}
+
+async function serialise_rendered(sugars,renderer,is_array_input=false) {
+  await BrowserShimmedSVGRenderer.AppendSymbols(renderer.element.canvas.querySelector('defs'));
+  let result = sugars.map( sugar => {
+    let target = renderer.element.canvas.cloneNode()
+    target.appendChild(renderer.element.canvas.querySelector('defs').cloneNode(true));
+    target.appendChild(renderer.rendered.get(sugar).element);
+    return target.outerHTML;
+  });
+  if ( ! is_array_input ) {
+    return result[0];
+  } else {
+    return result;
+  }
+}
+
+async function render_iupac_sugar(sequence='Man(a1-3)Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-O)Ser',options={linkage:false,oxford:false,rotate:false}) {
+
+  let sugars = parse_sequences(sequence);
+
+  let renderer = create_renderer_for_sugars(sugars,options.oxford ? LinkageLayout : SugarAwareLayout);
+
   renderer.LayoutEngine.LINKS = options.linkage ? true : false;
-  renderer.rotate = false;
+  renderer.rotate = options.rotate;
   renderer.refresh();
   renderer.scaleToFit();
-  return BrowserShimmedSVGRenderer.AppendSymbols(renderer.element.canvas.querySelector('defs')).then( () => {
-    
-    let result = sugars.map( sugar => {
-      let target = renderer.element.canvas.cloneNode()
-      target.appendChild(renderer.element.canvas.querySelector('defs').cloneNode(true));
-      target.appendChild(renderer.rendered.get(sugar).element);
-      return target.outerHTML;
-    });
 
-    if ( ! is_array_input ) {
-      return result[0];
-    } else {
-      return result;
-    }
+  return await serialise_rendered(sugars,renderer,Array.isArray(sequence));
 
-  });
 };
 
+async function render_iupac_sugar_fragment(sequence='Man(a1-3)Man(b1-4)GlcNAc(b1-4)GlcNAc(b1-O)Ser+"from(y2a)"',options={linkage:false,oxford:false,rotate:false}) {
 
-export { Glycan, render_iupac_sugar };
+  let is_array_input = Array.isArray(sequence);
+
+  let sequences = sequence;
+
+  if (! Array.isArray(sequence)) {
+    sequences = [sequence];
+  }
+
+  let sugars = parse_sequences(sequences.map( seq => seq.replace(/\+\".*/,'') ));
+
+  let renderer = create_renderer_for_sugars(sugars,options.oxford ? LinkageLayout : SugarAwareLayout);
+
+  renderer.LayoutEngine.LINKS = options.linkage ? true : false;
+  renderer.rotate = options.rotate;
+  renderer.refresh();
+
+  for (const [idx,identifier] of sequences.map( seq => seq.replace(/[^"]*\"/,'').replace('"','') ).entries()) {
+    let matches = identifier.match(/from\((.*)\)/);
+    if ( ! matches ) {
+      continue;
+    }
+    let position = matches[1];
+    const sugar = sugars[idx];
+    let wanted = sugar.composition(sugar.locate_monosaccharide(position));
+    let togo = sugar.composition().filter( res => wanted.indexOf(res) < 0 );
+    for (const res of togo) {
+      let rendered = renderer.rendered.get(res).residue;
+      let rendered_linkage = renderer.rendered.get(res).linkage;
+      if (rendered.element) {
+        rendered.element.parentNode.removeChild(rendered.element);
+        rendered.element.hidden = true;
+      }
+      if (rendered_linkage && rendered_linkage.parentNode) {
+        rendered_linkage.parentNode.removeChild(rendered_linkage);
+      }
+      if (rendered_linkage && rendered_linkage.element && rendered_linkage.element.parentNode) {
+        rendered_linkage.element.parentNode.removeChild(rendered_linkage.element);          
+      }
+
+      for (const kid of res.children) {
+        let kid_rendered = renderer.rendered.get(kid).linkage;
+        if (kid_rendered && kid_rendered.parentNode) {
+          kid_rendered.parentNode.removeChild(kid_rendered);          
+        }
+        if (kid_rendered && kid_rendered.element && kid_rendered.element.parentNode) {
+          kid_rendered.element.parentNode.removeChild(kid_rendered.element);          
+        }
+
+      }
+    }
+  } 
+
+  renderer.scaleToFit();
+
+  return await serialise_rendered(sugars,renderer,Array.isArray(sequence));
+
+
+}
+
+
+export { Glycan, render_iupac_sugar, render_iupac_sugar_fragment };
 
 
