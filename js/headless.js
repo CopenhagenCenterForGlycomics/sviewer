b3244c818950628d7fb8d49aabe5d82f90e77ff1
Hiren Joshi <hirenj@gmail.com>
2022-04-24 21:17:31 +0200

    Add in method exports from sviewer

--- js/headless.js
+++ js/headless.js
@@ -1,4 +1,6 @@
-import { Glycan, render_iupac_sugar } from './main-node';
+import { Glycan, render_iupac_sugar, load_sugar_svg, tagSupported } from './main-node';
 
 global.Glycan = Glycan;
 global.render_iupac_sugar = render_iupac_sugar;
+global.load_sugar_svg = load_sugar_svg;
+global.tagSupported = tagSupported;
